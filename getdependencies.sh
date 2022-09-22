d8464328030fa59036d7d5f4ff5d29d7631becd1
1795757 <111757853+1795757@users.noreply.github.com>
2022-09-22 15:40:37 -0400

    Update getdependencies.sh
    changed to just get azcopy and python dependencies

--- getdependencies.sh
+++ getdependencies.sh
@@ -3,13 +3,4 @@ wget -q -O azcopy_linux.tar.gz https://aka.ms/downloadazcopy-v10-linux
 tar -xvzf azcopy_linux.tar.gz --wildcards */azcopy --strip 1
 rm azcopy_linux.tar.gz
 chmod u+x azcopy
-# Download getdata
-wget -q -O getdata.sh
-chmod u+x getdata.sh
 pip install pandas Pillow torch torchvision tqdm 
-REPO="https://raw.githubusercontent.com/1795757/csl/main/"
-declare -a TODOWNLOAD=("unpack_format.py")
-for file in "${TODOWNLOAD[@]}"
-do
-  wget -q -O i "${REPO}${file}"
-done
