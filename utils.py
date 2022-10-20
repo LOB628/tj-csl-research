572596b2a6daffec93d94d96e0242536e1fc9d5c
1795757 <111757853+1795757@users.noreply.github.com>
2022-10-20 15:41:17 -0400

    Update and rename utils to utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+import os,shutil
 def ignore_files(dirname,lsdir):
   return [f for f in lsdir if os.path.isfile(f) or f.startswith(".")]
 def copy_folder_structure(name):
+  """util for copying folder structure without files
+     takes a name  and copies from name-images to name-processed
+  """
   shutil.copytree(f"{name}-images",f"{name}-images-processed",ignore=ignore_files)
