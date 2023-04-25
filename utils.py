c9a6d8b23faa5c6cbce63fe571af4a15bb3c09a5
1795757 <111757853+1795757@users.noreply.github.com>
2023-04-25 14:31:44 -0400

    moved toLabel and helper getitem_identity

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import os,shutil
+import os,shutil,torch
 def ignore_files(dirname,lsdir):
   return [f for f in lsdir if os.path.isfile(f) or f.startswith(".")]
 def copy_folder_structure(name):
@@ -6,3 +6,13 @@ def copy_folder_structure(name):
      takes a name  and copies from name-images to name-processed
   """
   shutil.copytree(f"{name}-images",f"{name}-images-processed",ignore=ignore_files)
+
+class getitem_identity:
+    def __init__(self):
+        pass
+    def __getitem__(self,key):
+        return key
+def toLabel(element,length=1,convert_category_id=getitem_identity:
+  ret=torch.zeros(length)
+  ret[convert_category_id[element]]=1
+  return ret  
